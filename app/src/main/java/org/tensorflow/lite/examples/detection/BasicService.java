9a34971350f272ef7d42b91953ff28093679826b
Mahesh.pc <mahesh@ratibpal.com>
2020-01-08 12:17:14 +0530

    #first_code

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/BasicService.java
@@ -0,0 +1,17 @@
+package org.tensorflow.lite.examples.detection;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class BasicService extends Service {
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
